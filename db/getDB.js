b5bc4b52c25825f52ce8fd74e77a97d8078a0437
Kaysera14 <131011220+Kaysera14@users.noreply.github.com>
2024-04-08 17:12:24 +0200

    fixed issue while creating db, added function to populate

--- db/getDB.js
+++ db/getDB.js
@@ -6,6 +6,15 @@ let pool;
 
 async function getDB() {
   if (!pool) {
+    const connection = await mysql.createConnection({
+      host: MYSQL_HOST,
+      user: MYSQL_USER,
+      password: MYSQL_PASSWORD,
+      timezone: 'Z',
+    });
+
+    await connection.query('CREATE DATABASE IF NOT EXISTS subrealista;');
+
     pool = mysql.createPool({
       connectionLimit: 10,
       host: MYSQL_HOST,
