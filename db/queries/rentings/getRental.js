1d83490d8c5e10293568252fe3fc8a211ea594cb
Kaysera14 <131011220+Kaysera14@users.noreply.github.com>
2024-05-07 15:42:37 +0200

    temp patch

--- db/queries/rentings/getRental.js
+++ db/queries/rentings/getRental.js
@@ -62,12 +62,16 @@ const getRental = async (id) => {
       throw generateError('Este alquiler no existe', 404);
     }
 
-    const finalResult = {
+    //objeto
+    /* const finalResult = {
       result: result[0],
       images: images,
       services: services,
       rentals: rentals,
-    };
+    }; */
+
+    // array
+    const finalResult = [result[0], images, services, rentals];
 
     return finalResult;
   } finally {
