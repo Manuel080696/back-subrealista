afe5e8723d0ae985ed7d233e9356ec05d472906b
Kaysera14 <131011220+Kaysera14@users.noreply.github.com>
2024-05-15 17:56:25 +0200

    deleted images and updated helpers

---
+++ helpers/createPathIfNotExists.js
@@ -0,0 +1,9 @@
+const fs = require('fs/promises');
+const createPathIfNotExists = async (path) => {
+  try {
+    await fs.access(path);
+  } catch {
+    await fs.mkdir(path);
+  }
+};
+module.exports = { createPathIfNotExists };
--- helpers/index.js
+++ helpers/index.js
@@ -1,4 +1,5 @@
 const { generateError } = require('./generateError');
+const { createPathIfNotExists } = require('./createPathIfNotExists');
 const sendMail = require('./sendMail');
 
-module.exports = { generateError, sendMail };
+module.exports = { generateError, sendMail, createPathIfNotExists };
